a0d8583cc9fa3377bd313ab9373a7b037fc055f4
Mosharof <md5470415@gmail.com>
2024-03-16 14:25:46 +0600

    js dom task 1 publish

---
+++ click.js
@@ -0,0 +1,10 @@
+const text = document.getElementById('changeText');
+
+const clickBtn = document.getElementById('clickBtn');
+
+clickBtn.addEventListener('click', function(){
+   text.innerHTML = `
+    <h3>আসসালামু ্  আলাইকুম!!</h3>
+    <img src="romjan3.jpg" alt="">
+   `;
+})
